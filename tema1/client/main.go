c4004ea975e7315ad8cb8fbfe8a3940f693998bf
Iordache Andrei Tudor <andrei.iordache2017@gmail.com>
2024-11-29 23:07:00 +0200

    Updates to server and client messages

--- tema1/client/main.go
+++ tema1/client/main.go
@@ -7,44 +7,65 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
+	"sync/atomic"
 )
 
-//to do:
-
-/*
-{
-   Problems: {
-    "problems": {
-      "problem1": {
-        "min_array_size": 3,
-        "max_array_size": 10
-      },
-      "problem6": {
-        "default_shift": "RIGHT",
-        "k": 3
-      },
-      "problem11": {
-        "k": 4
-      }
-    }
-  }
-}
-
-
-*/
-
 type Config struct {
-	Problem1 struct {
-		MinArraySize int `json:"min_array_size"`
-		MaxArraySize int `json:"max_array_size"`
-	} `json:"problem1"`
-	Problem6 struct {
-		DefaultShift int `json:"default_shift"` // 0 for LEFT, 1 for RIGHT
-		K            int `json:"k"`
-	} `json:"problem6"`
-	Problem11 struct {
-		K int `json:"k"`
-	} `json:"problem11"`
+	Problems struct {
+		Problem1 struct {
+			MinArraySize int      `json:"min_array_size"`
+			MaxArraySize int      `json:"max_array_size"`
+			InputArray   []string `json:"input_array"`
+		} `json:"problem1"`
+		Problem2 struct {
+			InputArray []string `json:"input_array"`
+		} `json:"problem2"`
+		Problem3 struct {
+			InputArray []int `json:"input_array"`
+		} `json:"problem3"`
+		Problem4 struct {
+			Range      []int `json:"range"`
+			InputArray []int `json:"input_array"`
+		} `json:"problem4"`
+		Problem5 struct {
+			InputArray []string `json:"input_array"`
+		} `json:"problem5"`
+		Problem6 struct {
+			DefaultShift string   `json:"default_shift"`
+			K            int      `json:"k"`
+			InputArray   []string `json:"input_array"`
+		} `json:"problem6"`
+		Problem7 struct {
+			EncodedText string `json:"encoded_text"`
+		} `json:"problem7"`
+		Problem8 struct {
+			InputArray []int `json:"input_array"`
+		} `json:"problem8"`
+		Problem9 struct {
+			InputArray []string `json:"input_array"`
+		} `json:"problem9"`
+		Problem10 struct {
+			InputArray []string `json:"input_array"`
+		} `json:"problem10"`
+		Problem11 struct {
+			K          int   `json:"k"`
+			InputArray []int `json:"input_array"`
+		} `json:"problem11"`
+		Problem12 struct {
+			InputArray []int `json:"input_array"`
+		} `json:"problem12"`
+		Problem13 struct {
+			Range        []int `json:"range"`
+			ComplexArray []int `json:"complex_array"`
+		} `json:"problem13"`
+		Problem14 struct {
+			InputArray []string `json:"input_array"`
+		} `json:"problem14"`
+		Problem15 struct {
+			InputArray []string `json:"input_array"`
+		} `json:"problem15"`
+	} `json:"problems"`
 }
 
 func readFromInputFile(path string) (Config, error) {
@@ -63,70 +84,118 @@ func readFromInputFile(path string) (Config, error) {
 	return config, nil
 }
 
-func writeLogToFile(path string, data string) error {
-	return nil
+func makeRequest(requestBody map[string]interface{}, serverAddress string) {
+
+	clientId := requestBody["client_id"].(int)
+
+	fullRequestBody := map[string]interface{}{
+		"connection_message": fmt.Sprintf("Client %d conectat", clientId),
+		"request_message":    fmt.Sprintf("Client %d a facut request cu datele: %v", clientId, requestBody["input_array"]),
+		"data":               requestBody,
+	}
+
+	data, err := json.Marshal(fullRequestBody)
+	if err != nil {
+		fmt.Println("Error marshalling full request body:", err)
+		return
+	}
+
+	res, err := http.Post(serverAddress, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	fmt.Println(fullRequestBody["connection_message"])
+	fmt.Println(fullRequestBody["request_message"])
+	defer res.Body.Close()
+
+	serializedData, err := io.ReadAll(res.Body)
+
+	mapResponse := map[string]interface{}{}
+
+	err = json.Unmarshal(serializedData, &mapResponse)
+
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	} else {
+		message := fmt.Sprintf("Clientul %v a primit raspunsul %v", clientId, string(mapResponse["result"].(string)))
+		fmt.Println(message)
+	}
+
 }
 
-func clientOne() {
-	//to do
+func clientOne(counter int, config Config) {
+
+	if config.Problems.Problem1.MinArraySize > len(config.Problems.Problem1.InputArray) || config.Problems.Problem1.MaxArraySize < len(config.Problems.Problem1.InputArray) {
+		return
+	}
+
+	words := config.Problems.Problem1.InputArray
+
+	requestBody := map[string]interface{}{
+		"problem":     "problem1",
+		"input_array": words,
+		"client_id":   counter,
+	}
+
+	makeRequest(requestBody, "http://localhost:8080/solve")
+
 }
 
-func clientTwo() {
-	//to do
+func clientTwo(counter int, config Config) {
+
+	if len(config.Problems.Problem2.InputArray) == 0 {
+		return
+	}
+
+	words := config.Problems.Problem2.InputArray
+
+	requestBody := map[string]interface{}{
+		"problem":     "problem2",
+		"input_array": words,
+		"client_id":   counter,
+	}
+
+	makeRequest(requestBody, "http://localhost:8080/solve")
+
 }
 
 func clientThree() {
 	//to do
 }
 
-func makeRequest(client func()) {
-	client()
-}
-
-func testRequest() {
-	data := map[string]string{
-		"name": "John",
-	}
-
-	// Marshal the map into JSON
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		fmt.Println("Error marshalling JSON:", err)
-		return
-	}
-
-	// Make the POST request
-	resp, err := http.Post("http://localhost:8080/test", "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("Error making POST request:", err)
-		return
-	}
-
-	// Read the response body
-	serializedData, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response:", err)
-		return
-	}
-
-	fmt.Println("Response from server:", string(serializedData))
-}
-
 func main() {
 
-	config, err := readFromInputFile("../clients_config.json")
+	var globalClientCounter int32 = 0
+
+	config := Config{}
+	config, err := readFromInputFile("/Users/andreiiordache/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Facultate/Anul III/Sisteme Distribuite /tema1/clients_config.json")
 	if err != nil {
-		fmt.Println("Error reading from file:", err)
+		fmt.Println("Error reading from input file:", err)
 		return
 	}
 
-	prettyPrint, err := json.MarshalIndent(config, "", "    ")
-	if err != nil {
-		fmt.Println("Error marshalling JSON:", err)
-		return
-	} else {
+	var wg sync.WaitGroup
 
-		fmt.Println(string(prettyPrint))
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			clientID := atomic.AddInt32(&globalClientCounter, 1)
+			clientOne(int(clientID), config)
+		}()
 	}
 
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			clientID := atomic.AddInt32(&globalClientCounter, 1)
+			clientTwo(int(clientID), config)
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("All requests completed.")
 }
--- tema1/server/main.go
+++ tema1/server/main.go
@@ -4,50 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 )
-
-func test(w http.ResponseWriter, r *http.Request) {
-
-	body, err := io.ReadAll(r.Body)
-
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
-	var data map[string]interface{}
-
-	err = json.Unmarshal(body, &data)
-
-	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	data["message"] = "Hello, " + data["name"].(string)
-
-	response, err := json.Marshal(data)
-
-	if err != nil {
-		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
-
-}
-
-func solve(w http.ResponseWriter, r *http.Request) ([]byte, error) {
-
-	request, err := io.ReadAll(r.Body)
-
-	return request, err
-
-}
 
 type Config struct {
 	Server struct {
@@ -74,9 +36,188 @@ func readFromInputFile(path string) (Config, error) {
 	return config, nil
 }
 
+func appendToResponse(response *map[string]interface{}, message string) {
+	(*response)["messages"] = append((*response)["messages"].([]string), message)
+}
+
+func Problem1(data map[string]interface{}) ([]string, error) {
+
+	inputArray, ok := data["input_array"]
+	if !ok {
+		message := "Error: Missing input_array"
+		return nil, fmt.Errorf(message)
+	}
+
+	array, ok := inputArray.([]interface{})
+	words := make([]string, len(array))
+	for i, v := range array {
+		word, ok := v.(string)
+		if !ok {
+			message := fmt.Sprintf("Error: input_array element at index %d is not a string", i)
+			return nil, fmt.Errorf(message)
+		}
+		words[i] = word
+	}
+
+	outputArray := make([]string, len(words[0]))
+	for i := 0; i < len(words[0]); i++ {
+		result := ""
+		for _, word := range words {
+			result += string(word[i])
+		}
+		outputArray[i] = result
+	}
+
+	return outputArray, nil
+}
+
+func Problem2(data map[string]interface{}) (string, error) {
+
+	inputArray, ok := data["input_array"]
+	if !ok {
+		return "", fmt.Errorf("Error: Missing input_array")
+	}
+
+	array, ok := inputArray.([]interface{})
+	if !ok {
+		return "", fmt.Errorf("Error: Invalid input_array format")
+	}
+
+	count := 0
+	re := regexp.MustCompile("[0-9]+")
+
+	for _, item := range array {
+
+		item, ok := item.(string)
+
+		if !ok {
+			return "", fmt.Errorf("Error: Invalid input_array format")
+		}
+
+		digits := re.FindAllString(item, -1)
+
+		for _, digit := range digits {
+			num, err := strconv.Atoi(digit)
+			if err != nil {
+				return "", fmt.Errorf("Error: Unable to convert digit '%s' to integer", digit)
+			}
+
+			sqrt := int(math.Sqrt(float64(num)))
+			if sqrt*sqrt == num {
+				count++
+			}
+		}
+	}
+
+	return fmt.Sprintf("%d perfect squares found", count), nil
+}
+
+func sendResponseWithJSON(w http.ResponseWriter, statusCode int, response map[string]interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response)
+
+	message := fmt.Sprintf("Serverul a trimis raspunsul %v", response["result"])
+	fmt.Println(message)
+
+}
+
+func solve(w http.ResponseWriter, r *http.Request) {
+	response := map[string]interface{}{
+		"messages": []string{},
+	}
+
+	appendToResponse(&response, "Server a primit requestul.")
+	fmt.Println("Serverul a primit requestul.")
+
+	data, err := handleRequest(r)
+	if err != nil {
+		appendToResponse(&response, "Error reading or parsing request.")
+		response["error"] = err.Error()
+		sendResponseWithJSON(w, http.StatusBadRequest, response)
+		return
+	}
+
+	appendToResponse(&response, "Server proceseaza datele.")
+	fmt.Println("Serverul proceseaza datele.")
+
+	result, err := processProblem(data, &response)
+	if err != nil {
+		response["error"] = err.Error()
+		sendResponseWithJSON(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	finalMessage := fmt.Sprintf("Server trimite '%s' catre client.", result)
+	appendToResponse(&response, finalMessage)
+	response["result"] = result
+
+	sendResponseWithJSON(w, http.StatusOK, response)
+}
+
+func processProblem(data map[string]interface{}, response *map[string]interface{}) (string, error) {
+	problem, ok := data["problem"].(string)
+	if !ok {
+		appendToResponse(response, "Invalid or missing 'problem' key.")
+		return "", fmt.Errorf("invalid or missing 'problem' key")
+	}
+
+	switch problem {
+	case "problem1":
+		outputArray, err := Problem1(data)
+		if err != nil {
+			appendToResponse(response, "Error solving problem1.")
+			return "", err
+		}
+		return fmt.Sprintf("%v", outputArray), nil
+	case "problem2":
+		result, err := Problem2(data)
+		if err != nil {
+			appendToResponse(response, "Error solving problem2.")
+			return "", err
+		}
+		appendToResponse(response, "Problem2 processed successfully.")
+		return result, nil
+	case "problem3":
+		//to do
+		appendToResponse(response, "Problem3 processed successfully.")
+		return "Problem3 solved (example placeholder)", nil
+	default:
+		appendToResponse(response, "Unknown problem.")
+		return "", fmt.Errorf("unknown problem")
+	}
+}
+
+func handleRequest(r *http.Request) (map[string]interface{}, error) {
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading request body: %v", err)
+	}
+
+	var fullData map[string]interface{}
+	if err := json.Unmarshal(body, &fullData); err != nil {
+		return nil, fmt.Errorf("invalid JSON: %v", err)
+	}
+
+	data, ok := fullData["data"].(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("missing 'data' key or invalid format in request")
+	}
+
+	if _, ok := data["problem"]; !ok {
+		return nil, fmt.Errorf("missing 'problem' key in request")
+	}
+	if _, ok := data["client_id"]; !ok {
+		return nil, fmt.Errorf("missing 'client_id' key in request")
+	}
+
+	return data, nil
+}
+
 func main() {
 
-	http.HandleFunc("/test", test)
+	http.HandleFunc("/solve", solve)
 
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
